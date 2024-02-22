842a586d9d5008ba0643e97bcd7be6aef126e084
Nhu Dang <115023003+NhuDang1604@users.noreply.github.com>
2024-02-22 16:18:06 +0700

    Add files via upload

---
+++ if.js
@@ -0,0 +1,20 @@
+var fullname = 1>2;
+if (fullname) {
+    console.log('Dieu kien dung');
+    } else {
+        console.log('Dieu kien sai');
+    }
+
+var a = 1;
+var b = 2;
+var c = 3;
+if (!(a<0)) {
+console.log('dieu kien dung');
+}
+
+var a = true;
+var b = false;
+var c = a || b;
+var d = b && c;
+
+console.log(c, d); // Output: ?
---
+++ main.js
@@ -0,0 +1,29 @@
+
+// var hi = "Nhu Dang"
+// window.alert(hi);/**
+// day la cach ghi
+// chu nhiu dong trong vsc */
+//cach nay cmt 1 dong
+/* 
+cach nay cung duoc ne
+*/
+//built in la gi
+/**
+ * gioi thieu mot so ham build in
+ * 
+ */
+// console.log('hehe') //sysout in java
+// //Toantu
+// var number = 6
+// var output = number++ + --number; //6+6
+// console.log('output:', output);
+// tien to : lay kq sau khi tinh'
+// hau to: lay kq truoc khi tinh'
+var firstName = 'Dang'
+var lastName = 'Nhu'
+
+// console.log(firstName + ' ' + lastName);
+var name = 'Son'
+name += +  'Dang'
+console.log(name)
+//output => SOnDang
